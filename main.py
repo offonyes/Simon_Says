daa971b041cb4366df05eeab90388e13ae8ed2d6
Dimitri_Kat <49370568+offonyes@users.noreply.github.com>
2023-03-23 00:31:49 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame, random
 from sprites import *
 from setting import *
-
+status = "New_open"
+logo = pygame.image.load("download-removebg-preview.png")
+logo = pygame.transform.scale(logo,(600,90))
 class Game:
     def __init__(self): 
         pygame.init()
@@ -11,11 +13,12 @@ class Game:
         self.colours = [GREEN,RED, YELLOW, BLUE]
         self.dark_colours = [DARKGREEN,DARKRED, DARKYELLOW, DARKBLUE]
         self.flash = [Flash(0,0,WIDTH,HEIGHT,WHITE,3,self.screen)]
-        self.buttons = [Button(110,30,DARKGREEN),Button(330,30,DARKRED),Button(110,250,DARKYELLOW),Button(330,250,DARKBLUE)]
+        self.buttons = [Button(110,30,DARKGREEN,SIZE_OF_BUTTON,SIZE_OF_BUTTON),Button(330,30,DARKRED,SIZE_OF_BUTTON,SIZE_OF_BUTTON),Button(110,250,DARKYELLOW,SIZE_OF_BUTTON,SIZE_OF_BUTTON),Button(330,250,DARKBLUE,SIZE_OF_BUTTON,SIZE_OF_BUTTON)]
         self.sound = [pygame.mixer.Sound(i) for i in audio_files]
         [self.sound[i].set_volume(0.015) for i in range(4)]
         self.loss_sound = pygame.mixer.Sound("Sound\Loss_sound.wav")
         self.loss_sound.set_volume(0.015)
+        self.start_button = Button(100,250,DARKGREEN,440,100)
 
     def get_max_score(self): 
         with open("max_score.txt", "r") as file:
@@ -44,6 +47,16 @@ class Game:
             self.draw()
             self.update()
 
+    def start(self):
+        self.max_score = self.get_max_score()
+        self.screen.fill((40, 40, 40))
+        self.screen.blit(logo,(20,50))
+        self.start_button.draw(self.screen)
+        TextOnScreen(230, 270, "START",70,RED).draw(self.screen)
+        TextOnScreen(440, 480, f"High score: {str(self.max_score)}",24,WHITE).draw(self.screen)
+        pygame.display.update()
+        self.events()
+        
     def update(self):
         if not self.waiting_player:
             pygame.time.wait(1000) #time between stages
@@ -65,14 +78,15 @@ class Game:
 
             elif self.clicked_button and self.clicked_button != self.order[self.current_step]:
                 self.loss_sound.play()
+                pygame.time.wait(200)
                 self.flash[0].animation()
                 self.save_score()
                 self.playing = False
 
     def draw(self):
         self.screen.fill((40, 40, 40))
-        TextOnScreen(170, 480, f"Score: {str(self.score)}").draw(self.screen)
-        TextOnScreen(370, 480, f"High score: {str(self.max_score)}").draw(self.screen)
+        TextOnScreen(170, 480, f"Score: {str(self.score)}",16,WHITE).draw(self.screen)
+        TextOnScreen(370, 480, f"High score: {str(self.max_score)}",16,WHITE).draw(self.screen)
         for button in self.buttons:
             button.draw(self.screen)
         pygame.display.update()
@@ -87,6 +101,7 @@ class Game:
         button_flash.animation()
 
     def events(self): 
+        global status
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -96,10 +111,17 @@ class Game:
                 for button in self.buttons:
                     if button.clicked(mouse_x, mouse_y):
                         self.clicked_button = button.colour
+                if self.start_button.clicked(mouse_x, mouse_y):
+                    self.new()
+                    self.run()
+                    status = "Play"
         
 
 game = Game()
 
 while True:
-    game.new()
-    game.run()
+    if status == "New_open":
+        game.start()
+    if status == "Play":
+        game.new()
+        game.run()
--- sprites.py
+++ sprites.py
@@ -3,25 +3,28 @@ from setting import *
 clock = pygame.time.Clock()
 #button class
 class Button:
-    def __init__(self, x, y, colour):
+    def __init__(self, x, y, colour,size_x,size_y):
         self.x, self.y = x, y
         self.colour = colour
+        self.size_x, self.size_y = size_x,size_y
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.colour,(self.x, self.y, SIZE_OF_BUTTON, SIZE_OF_BUTTON))
+        pygame.draw.rect(screen, self.colour,(self.x, self.y, self.size_x, self.size_y))
 
     def clicked(self, mouse_x, mouse_y):
-        return self.x <= mouse_x <= self.x + SIZE_OF_BUTTON and self.y <= mouse_y <= self.y + SIZE_OF_BUTTON
+        return self.x <= mouse_x <= self.x + self.size_x and self.y <= mouse_y <= self.y + self.size_y
 
 
 class TextOnScreen:
-    def __init__(self, x, y, text):
+    def __init__(self, x, y, text,size,color):
         self.x, self.y = x, y
         self.text = text
+        self.size = size
+        self.color = color
 
     def draw(self, screen):
-        font = pygame.font.SysFont("Consolas", 16)
-        text = font.render(self.text, True, WHITE)
+        font = pygame.font.SysFont("Consolas", self.size)
+        text = font.render(self.text, True, self.color)
         screen.blit(text, (self.x, self.y))
 
 class Flash:

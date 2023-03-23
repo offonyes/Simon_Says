4cfcda68ca13db2e33b5531d612397fe703df5bf
Dimitri_Kat <49370568+offonyes@users.noreply.github.com>
2023-03-23 22:38:34 +0400

    Add files via upload (#3)

--- setting.py
+++ setting.py
@@ -2,14 +2,13 @@
 WHITE = (255, 255, 255)
 DARKGREY = (40, 40, 40)
 GREEN = (0, 255, 0)
-DARKGREEN = (0, 155, 0)
+DARKGREEN = (0, 155, 0) #009b00
 RED = (255, 0, 0)
-DARKRED = (155, 0, 0)
+DARKRED = (155, 0, 0) #9b0000
 YELLOW = (255, 255, 0)
-DARKYELLOW = (155, 155, 0)
+DARKYELLOW = (155, 155, 0) #9b9b00
 BLUE = (0, 0, 255)
-DARKBLUE = (0, 0, 155)
-
+DARKBLUE = (0, 0, 155) #00009b
 
 # game settings
 WIDTH = 640
@@ -18,5 +17,7 @@ FPS = 60
 TITLE = "Simon Says"
 SIZE_OF_BUTTON = 200
 ANIMATION_SPEED = 10
+status = "New_open"
+
 
 audio_files = ["Sound\Green_audio.wav","Sound\Red_audio.wav","Sound\Yellow_audio.wav","Sound\Blue_audio.wav"]
